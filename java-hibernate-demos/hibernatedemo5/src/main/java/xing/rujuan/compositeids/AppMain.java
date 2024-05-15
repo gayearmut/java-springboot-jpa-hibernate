010b1db47da10380ff9b4f996ea8f428081625ff
Gaye Armut <57320188+gayearmut@users.noreply.github.com>
2024-05-15 14:19:40 +0300

    Add hibernatedemo5

---
+++ java-hibernate-demos/hibernatedemo5/src/main/java/xing/rujuan/compositeids/AppMain.java
@@ -0,0 +1,37 @@
+package xing.rujuan.compositeids;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
+public class AppMain {
+
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("edu.mum.cs");
+
+    public static void main(String[] args) {
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Name name = new Name("Gaye", "Armut");
+        Person p = new Person(name, LocalDate.of(2024, 05, 15));
+
+
+        Address address = new Address("1000 N 4th", "52556");
+        p.addAddress(address);
+        Address address2 = new Address("1001 N 4th", "12556");
+        p.addAddress(address2);
+
+        em.persist(p);
+
+//        Name name2 = new Name("Tina", "Xing");
+//        Person p2 = new Person(name2, LocalDate.of(2018, 01, 01));
+//        em.persist(p2);
+
+        em.getTransaction().commit();
+        em.close();
+
+    }
+
+}
---
+++ java-hibernate-demos/hibernatedemo5/src/main/java/xing/rujuan/compositeids/Person.java
@@ -0,0 +1,40 @@
+package xing.rujuan.compositeids;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class Person {
+
+    @EmbeddedId
+    private Name name;
+
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "person", cascade = CascadeType.PERSIST)
+    private List<Address> addresses = new ArrayList<>();
+
+
+    public Person(Name name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public void addAddress(Address address){
+        addresses.add(address);
+        address.setPerson(this);
+    }
+
+}
